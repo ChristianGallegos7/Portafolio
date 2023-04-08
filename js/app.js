d48219ca7fb27dccef3e8568b0e5fb2bd2c94f2d
Chris Razor <getworkchris@gmail.com>
2023-04-08 08:12:58 -0500

    Faltan media querys

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('.header', { delay: 500 });
+sr.reveal('.main', { delay: 500 });
+sr.reveal('.skills', { delay: 500 });
+sr.reveal('.aboutme', { delay: 500 });
+sr.reveal('.projects', { delay: 500 });
+sr.reveal('.contact', { delay: 500 });
+
